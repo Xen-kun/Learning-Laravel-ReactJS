16e615bd9dea1926dcdae20fe9a9946654fc1223
Xen-kun <stevedaleja2001@gmail.com>
2025-02-25 01:14:43 +0800

    changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
-     */     
+     */
     public function index()
     {
         return inertia('Home');
